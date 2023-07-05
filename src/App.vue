6ea154d34a2736e609bdc7354e13ed7b39b37503
Pauliina <Pauliina@AWA5CD2116DNV>
2023-07-05 11:27:07 +0300

    sss

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="layout">
+  <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Events</RouterLink> | 
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+  </div>
+</template>
+
+<style>
+#layout
+{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color:#2c3e50
+}
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+
+nav {
+padding: 20px;
+}
+
+nav a {
+  font-weight: bold;
+  color:#2c3e50
+}
+
+nav a.router-link-exact-active {
+  color: #42b983
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+
+h2{
+  font-size: 20;
+  color:blueviolet
+}
+</style>
---
+++ src/views/EventListView.vue
@@ -0,0 +1,69 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+import { ref } from 'vue';
+const events=ref([
+  {
+  id:5928101,
+  category:'animal welfare',
+  title:'Feed the animals',
+  description:'Feed all animals.',
+  location:'Meow Town',
+  date:'January 28, 2023',
+  time:'12:00',
+  petsAllowed:true,
+  organizwr:'Kat Laydee'
+  },
+  {
+  id:5928101,
+  category:'animal welfare',
+  title:'Feed the animals',
+  description:'Feed all animals.',
+  location:'Meow Town',
+  date:'January 28, 2023',
+  time:'12:00',
+  petsAllowed:true,
+  organizwr:'Kat Laydee'
+  },
+  {
+  id:5928101,
+  category:'animal welfare',
+  title:'Feed the animals',
+  description:'Feed all animals.',
+  location:'Meow Town',
+  date:'January 28, 2023',
+  time:'12:00',
+  petsAllowed:true,
+  organizwr:'Kat Laydee'
+  },
+  {
+  id:5928101,
+  category:'animal welfare',
+  title:'Feed the animals',
+  description:'Feed all animals.',
+  location:'Meow Town',
+  date:'January 28, 2023',
+  time:'12:00',
+  petsAllowed:true,
+  organizwr:'Kat Laydee'
+  },
+
+])
+
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+
+.events{
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
